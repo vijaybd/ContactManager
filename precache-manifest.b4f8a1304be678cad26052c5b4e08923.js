44b55e7b6766e1832931032e8632647d78f7c915
Vijay <vijayadevadiga1998@gmail.com>
2020-04-21 13:59:28 +0530

    Updates

---
+++ precache-manifest.b4f8a1304be678cad26052c5b4e08923.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d53836e2e419971df89068f930cdcf3",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "53cd878767414fa1ec73",
+    "url": "/ContactManager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "dd13a19eb9bc5d3f01a9",
+    "url": "/ContactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "53cd878767414fa1ec73",
+    "url": "/ContactManager/static/js/2.8a6ad5d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactManager/static/js/2.8a6ad5d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd13a19eb9bc5d3f01a9",
+    "url": "/ContactManager/static/js/main.0ddeec55.chunk.js"
+  },
+  {
+    "revision": "8b7c913f2e5670d9d5dc",
+    "url": "/ContactManager/static/js/runtime-main.19cae20d.js"
+  }
+]);
